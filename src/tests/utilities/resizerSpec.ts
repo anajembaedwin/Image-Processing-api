8c900ff97d3d0577cc9abf8cbc1b57edafe6e215
iSommie <anajembaedwin@gmail.com>
2023-01-04 20:47:15 +0100

    prettier and lint ran succesfully after test

--- src/tests/utilities/resizerSpec.ts
+++ src/tests/utilities/resizerSpec.ts
@@ -5,31 +5,31 @@ import * as path from 'path';
 
 describe('sharp', () => {
   it('should return image metadata', async () => {
-  // Set the path for the image file
-  const imagePath = path.resolve(__dirname, '../../../images/full/fjord.jpg');
-  
-  // Read the image file
-  const image = sharp(imagePath);
-  
-  // Get image metadata
-  const metadata = await image.metadata();
-  
-  // Verify the metadata
-  expect(metadata).toBeDefined();
-  expect(metadata.format).toBe('jpeg');
+    // Set the path for the image file
+    const imagePath = path.resolve(__dirname, '../../../images/full/fjord.jpg');
+
+    // Read the image file
+    const image = sharp(imagePath);
+
+    // Get image metadata
+    const metadata = await image.metadata();
+
+    // Verify the metadata
+    expect(metadata).toBeDefined();
+    expect(metadata.format).toBe('jpeg');
   });
-  
+
   it('should return a resized image', async () => {
-  // Set the path for the image file
-  const imagePath = path.resolve(__dirname, '../../../images/full/fjord.jpg');
-  
-  // Read the image file
-  const image = sharp(imagePath);
-  
-  // Resize the image
-  const resizedImage = image.resize(200, 200);
-  
-  // Verify the resized image
-  expect(resizedImage).toBeDefined();
+    // Set the path for the image file
+    const imagePath = path.resolve(__dirname, '../../../images/full/fjord.jpg');
+
+    // Read the image file
+    const image = sharp(imagePath);
+
+    // Resize the image
+    const resizedImage = image.resize(200, 200);
+
+    // Verify the resized image
+    expect(resizedImage).toBeDefined();
   });
-  });
+});
